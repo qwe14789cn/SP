a8b19797663589a1c69e2ec45b6a763741106f10
qwe14789cn <qwe14789cn@gmail.com>
2024-12-20 11:48:49 +0800

    Update auto_fusion.m
    连通域合并函数

--- +sp/auto_fusion.m
+++ +sp/auto_fusion.m
@@ -1,5 +1,5 @@
 %--------------------------------------------------------------------------
-%   [output] = auto_fusion(input_data,conn)
+%   [output,pix_num] = auto_fusion(input_data,conn)
 %--------------------------------------------------------------------------
 %   功能：
 %   寻找在二进制图像中的最大连通域并给出连通域的质心,图像，雷达中比较常用
@@ -16,17 +16,19 @@
 %                                 x1      y1
 %                                 x2      y2
 %                                ....    ....
+%           pix_num             像素数
 %--------------------------------------------------------------------------
 %   例子:
 %   test = peaks>2;
 %   auto_fusion(test,8)
 %--------------------------------------------------------------------------
-function [output] = auto_fusion(input_data,conn)
+function [output,pix_num] = auto_fusion(input_data,conn)
 CC = bwconncomp(input_data,conn);
 N = CC.NumObjects;
 output = zeros(N,2);
 S = regionprops(CC,'Centroid');
 for idx = 1:N
     output(idx,:) = S(idx).Centroid;
+    pix_num(idx,:) = numel(CC.PixelIdxList{idx});
 end
 end
