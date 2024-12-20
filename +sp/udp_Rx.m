fa03de6a32ae2efca577f77ab5bdbaa15801da89
qwe14789cn <qwe14789cn@gmail.com>
2024-12-20 12:00:06 +0800

    Update udp_Rx.m
    udp数据接收

--- +sp/udp_Rx.m
+++ +sp/udp_Rx.m
@@ -7,17 +7,19 @@
 %   输入：
 %           port                端口号
 %           package_size        数据包大小格式
+%           data_type           数据形式
 %--------------------------------------------------------------------------
 %   例子：
-%   R = udp_Rx(1234,[10,5])
+%   R = udp_Rx(1234,[10,5],'uint8')
 %--------------------------------------------------------------------------
-function Received = udp_Rx(port,package_size)
-udpRx = dsp.UDPReceiver('MessageDataType','uint8');
+function Received = udp_Rx(port,package_size,data_type)
+udpRx = dsp.UDPReceiver('MessageDataType',data_type);
 udpRx.LocalIPPort = port;
 udpRx.ReceiveBufferSize = 65536;
 udpRx.MaximumMessageLength = package_size(1);
 TOTAL = package_size(2);
-Received = uint8(zeros(package_size(1),package_size(2)));
+Received = zeros(package_size(1),package_size(2));
+eval(['Received = ' data_type '(Received);']);
 while TOTAL
     Rx = udpRx();
     if ~isempty(Rx)
