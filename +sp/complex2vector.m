60832cde92a48ba66face40e6601676c4e1eed95
qwe14789cn <qwe14789cn@gmail.com>
2024-12-20 11:52:04 +0800

    Update complex2vector.m
    复矢量可视化

--- +sp/complex2vector.m
+++ +sp/complex2vector.m
@@ -14,12 +14,15 @@
 %--------------------------------------------------------------------------
 function complex2vector(points,model)
 if nargin == 1
+    model = '2D';
+end
+if upper(model) == "2D"
     for idx = 1:numel(points)
         quiver(0,0,real(points(idx)),imag(points(idx)));
         text(real(points(idx)),imag(points(idx)),num2str(idx));
         hold on
     end
-elseif model == '3d'
+elseif upper(model) == "3D"
     for idx = 1:numel(points)
         quiver3(idx,0,0,   0,real(points(idx)),imag(points(idx)));
         text(idx,real(points(idx)),imag(points(idx)),num2str(idx));
@@ -27,6 +30,6 @@ elseif model == '3d'
     end
     plot3(1:numel(points),zeros(1,numel(points)),zeros(1,numel(points)),'k','LineWidth',1)
     plot3(1:numel(points),zeros(1,numel(points)),zeros(1,numel(points)),'ro','LineWidth',1)
-
 end
+
 hold off;grid on;
