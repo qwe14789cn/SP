bf7aff25d61c6d4ca69e3cb1f9a8d47ca2a41e24
qwe14789cn <qwe14789cn@gmail.com>
2025-04-03 17:22:15 +0800

    Update complex_randn.m
    复噪声

--- +sp/complex_randn.m
+++ +sp/complex_randn.m
@@ -1,22 +1,26 @@
 %--------------------------------------------------------------------------
-%   output = complex_randn(in,in2)
+%   output = complex_randn(varargin)
 %--------------------------------------------------------------------------
 %   功能：
 %   复噪声高斯分布随机生成器,单位为1
 %--------------------------------------------------------------------------
 %   输入：
-%           in          方阵长度
-%           in2         矩阵长度
+%           varargin          数据长度
 %--------------------------------------------------------------------------
 %   例子：
 %   complex_randn(5)
 %   complex_randn(10,20)
 %--------------------------------------------------------------------------
-function output = complex_randn(in,in2)
-if nargin ==1
-    output = 1./sqrt(2)*(randn(in) + 1j.*randn(in));
-else
-    output = 1./sqrt(2)*(randn(in,in2) + 1j.*randn(in,in2));
+function output = complex_randn(varargin)
+str_input = [];
+for idx = 1:nargin
+    if idx == nargin
+        str_input = [str_input num2str(varargin{idx})];
+    else
+        str_input = [str_input num2str(varargin{idx}),','];
+    end
 end
+
+output = eval(['1/sqrt(2).*(randn([' str_input '])+1j.*randn([' str_input ']))']);
     
 end
