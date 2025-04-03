5612664c17914b0650229dbbc89c294aab2a300f
qwe14789cn <qwe14789cn@gmail.com>
2025-04-03 17:25:22 +0800

    Update prt2axis_v.m
    prt计算轴

--- +sp/prt2axis_v.m
+++ +sp/prt2axis_v.m
@@ -1,23 +1,27 @@
 %--------------------------------------------------------------------------
-%   v_axis = prt2axis_v(prt,lambda,N)
+%   [v_axis,res_v] = prt2axis_v(prt,fc,N)
 %--------------------------------------------------------------------------
 %   功能:
 %   根据雷达发射的PRT时间和载波频率及N点fft点数计算多普勒速度轴
 %--------------------------------------------------------------------------
 %   输入:
 %           prt             脉冲间隔时间 s
-%           lambda          发射波长
+%           fc              发射频率
 %           N               做fft的点数
 %   输出:
 %           v_axis          速度轴 m/s
+%           res_v           速度最小分辨单元
 %--------------------------------------------------------------------------
-function v_axis = prt2axis_v(prt,lambda,N)
-PRF = 1/prt;vmax = PRF*lambda/2;
+function [v_axis,res_v] = prt2axis_v(prt,fc,N)
+lambda = 3e8/fc;
+PRF = 1/prt;
+vmax = PRF*lambda/2;
+res_v = vmax/N;
+disp(['速度最小分辨单元:' num2str(res_v) ' m/s'])
 if rem(N,2) == 0
-    v_axis = fftshift((0:N-1)*vmax./N - vmax/2);
+    v_axis = (0:N-1)*res_v - vmax/2;
 else
-    v_axis = (0:N-1)*vmax./N - vmax/N*(N-1)/2;
-    v_axis = [v_axis(((N+1)/2):end) v_axis(1:(N-1)/2)];
+    v_axis = (0:N-1)*res_v - res_v*(N-1)/2;
 end
 v_axis = v_axis(:);
 end
